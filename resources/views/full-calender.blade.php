94bdc246c3ebe1e6fd62f3807e56391e8a0d8cae
Samuel Luis <aluno20461@ipt.pt>
2021-09-02 19:35:06 +0100

    calendario com botão

--- resources/views/full-calender.blade.php
+++ resources/views/full-calender.blade.php
@@ -1,16 +1,27 @@
-@extends('master')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How to Use Fullcalendar in Laravel 8</title>
+    
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-@section('title')
-    Dashboard
-@endsection
-@section('content')
-
-<div class="content">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
+</head>
+<body>
+  
+<div class="container">
+    <form method="get" action="/dashboard">
+        <button type="submit">Back</button>
+    </form>
     <div id="calendar"></div>
-</div>
-@endsection  
-@section('scripts')
 
+</div>
+ 
+<script>
 $(document).ready(function () {
 
     $.ajaxSetup({
@@ -128,4 +139,7 @@ $(document).ready(function () {
 
 });
   
-@endsection
+</script>
+  
+</body>
+</html>

b9bc7ad4ed8cb8fe85e4558bfe118a0733a5abd1
Samuel Luis <aluno20461@ipt.pt>
2021-10-12 16:03:50 +0100

    merge

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::get('/details/{id}',[DetailsController::class,'getId'])->name('details.ge
 // search
 Route::get('/search1',[SeachController::class,'getallTickets'])->name('SeachController.getallTickets');
 // Rotas tickets
+Route::get('/ticket',[TicketController::class,'index'])->name('ticket.getAllTicketsNew');
+Route::get('/ticket2',[TicketController::class,'index'])->name('ticket.getAllTicketsNew2');
 Route::get('/ticket3',[TicketController::class,'index'])->name('ticket.getAllTicketsNew3');
 // Route::get('/ticket/{state}',[TicketController::class,'getState'])->name('ticket.getState');
 // Route::get('/ticket/{QueueIDs}/{state}',[TicketController::class,'getQueueStatee'])->name('ticket.getQueueStatee');

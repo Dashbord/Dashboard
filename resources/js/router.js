7200def7a0dcb769e6ece79e294651ce9381f388
Pedro Vinha <aluno20179@ipt.pt>
2021-10-14 17:35:42 +0100

    merge

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Dashboard from './views/Dashboard';
-<<<<<<< Updated upstream
-=======
-import StatusView from './views/StatusView/StatusView';
-import StatusViewClose from './views/StatusView/StatusViewClose';
->>>>>>> Stashed changes
 import Details from './views/Details';
 import Search1 from './views/Search/Search1';
 
@@ -31,17 +26,6 @@ export default new VueRouter({
             meta: { title: 'Dashboard' }
         },
         {
-<<<<<<< Updated upstream
-=======
-            path: '/StatusView', name: 'StatusView', component: StatusView,
-            meta: { title: 'StatusView' }
-        },
-        {
-            path: '/StatusViewClose', name: 'StatusViewClose', component: StatusViewClose,
-            meta: { title: 'StatusViewClose' }
-        },
-        {
->>>>>>> Stashed changes
             path: '/Details/:id', name: 'Details', component: Details, props:true,
             
         },
